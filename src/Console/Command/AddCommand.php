629b8c291eedbf97ec1d6dfdd1f8bc9a6543278e
mati <mateusz.koruna@digitalholding.pl>
2020-08-24 16:15:37 +0200

    WIP AddCommand

---
+++ src/Console/Command/AddCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace DH\ArtisPackageManagerBundle\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Traitor\Traitor;
+
+final class AddCommand extends Command
+{
+    protected static $defaultName = 'artis:add-traits';
+
+    /** @var Traitor */
+    private $traitor;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->traitor = new Traitor();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Setting up traits.')
+            ->setHelp('This command allows you to set up traits...')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
+}
