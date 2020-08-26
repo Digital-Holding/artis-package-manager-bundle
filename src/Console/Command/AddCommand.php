1c571f6ba90b7c85d9dc73c1b278bd4a3135670f
mati <mateusz.koruna@digitalholding.pl>
2020-08-26 12:07:42 +0200

    add traits based on config

--- src/Console/Command/AddCommand.php
+++ src/Console/Command/AddCommand.php
@@ -7,6 +7,7 @@ namespace DH\ArtisPackageManagerBundle\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Traitor\Traitor;
 
 final class AddCommand extends Command
@@ -16,30 +17,40 @@ final class AddCommand extends Command
     /** @var Traitor */
     private $traitor;
 
-    public function __construct()
+    /** @var ParameterBagInterface */
+    private $parameterBag;
+
+    public function __construct(ParameterBagInterface $parameterBag)
     {
         parent::__construct();
 
         $this->traitor = new Traitor();
+        $this->parameterBag = $parameterBag;
     }
 
     protected function configure(): void
     {
         $this
             ->setDescription('Setting up traits.')
-            ->setHelp('This command allows you to set up traits...')
-        ;
+            ->setHelp('This command allows you to set up traits...');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $class = '';
-        $traitClass = '';
+        $projectDir = $this->parameterBag->get('kernel.project_dir');
 
-        $traitAssigned = $this->traitor->alreadyUses($class, $traitClass);
+        $configPath = $projectDir . '/src/Resources/config/artis_package_manager_config.json';
+        $configFile = file_get_contents($configPath);
+        $config = json_decode($configFile, true);
 
-        if (!$traitAssigned) {
-            $this->traitor->addTrait($traitClass)->toClass($class);
+        foreach ($config['install'] as $entity => $traits) {
+            foreach ($traits['add'] as $trait) {
+                $traitAssigned = $this->traitor->alreadyUses($entity, $trait);
+
+                if (!$traitAssigned) {
+                    $this->traitor->addTrait($trait)->toClass($entity);
+                }
+            }
         }
     }
 }
